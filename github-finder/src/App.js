b305a16800c5b89f2c58435660a7346d64f680b8
Marc Vilà Espuña <marcvilaespunya@gmail.com>
2022-03-13 20:11:01 +0100

    Initial settings, creating layout and pages, routing

---
+++ github-finder/src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+import {BrowserRouter as Router,Route,Routes} from 'react-router-dom';
+import  Navbar  from "./components/layout/Navbar.jsx";
+import  Footer  from "./components/layout/Footer.jsx";
+import  Home  from "./components/pages/Home";
+import  About  from "./components/pages/About";
+import  NotFound  from "./components/pages/NotFound";
+
+function App() {
+  return (
+    <Router>
+      <div className='flex flex-col justify-between h-screen'>
+        <Navbar/>
+       <main className='container mx-auto px-3 pb-12'>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/home' element={<Home/>} />
+          <Route path='/about' element={<About/>} />
+          <Route path='/notound' element={<NotFound/>} />
+          <Route path='/*' element={<NotFound/>} />
+        </Routes>
+       </main>
+        <Footer/>
+      </div>
+    </Router>
+  
+  );
+}
+
+export default App;
